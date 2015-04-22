8225997cce9b6aec1a850aa43717b0f4f2b8fad3
unknown <rencie.bautista@yahoo.com>
2015-04-22 10:18:43 +0800

    1

--- app/controllers/DownloadsController.php
+++ app/controllers/DownloadsController.php
@@ -30,22 +30,24 @@ class DownloadsController extends \BaseController {
 				->where('cycle_id',$cycle->id)
 				->where('status_id',8)
 				->get();
-			$nofile = 'public/nofile/robots.txt';
-			if(count($activities) > 0){
-				foreach ($activities as $activity) {
-					$path = '/uploads/'.$activity->cycle_id.'/'.$activity->activity_type_id.'/'.$activity->id;
-					$distination = storage_path().$path ;
-					$files = File::files($distination);
-					if(count($files)>0){
-						$folder = 'app/storage/'.$path.'/';
-					}else{
-						$folder = $nofile;
-					}
-					$folders[strtoupper(Helper::sanitize($type->activity_type)).'/'.$activity->id.'_'.strtoupper(Helper::sanitize($activity->circular_name))] = $folder;
-				}
-			}else{
-				$folders[strtoupper(Helper::sanitize($type->activity_type))] = $nofile;
-			}
+
+			$nofile = 'public/nofile';
+			// if(count($activities) > 0){
+			// 	foreach ($activities as $activity) {
+			// 		$path = '/uploads/'.$activity->cycle_id.'/'.$activity->activity_type_id.'/'.$activity->id;
+			// 		$distination = storage_path().$path ;
+			// 		$files = File::files($distination);
+			// 		if(count($files)>0){
+			// 			$folder = 'app/storage/'.$path.'/';
+			// 		}else{
+			// 			$folder = $nofile;
+			// 		}
+			// 		$folders[strtoupper(Helper::sanitize($type->activity_type)).'/'.$activity->id.'_'.strtoupper(Helper::sanitize($activity->circular_name))] = $folder;
+			// 	}
+			// }else{
+			// 	$folders[strtoupper(Helper::sanitize($type->activity_type))] = $nofile;
+			// }
+			$folders[strtoupper(Helper::sanitize($type->activity_type))] = $nofile;
 		}
 		// Helper::print_array($folders);
 		$archive = $zippy->create($zip_path,$folders);
