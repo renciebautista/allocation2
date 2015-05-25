88c37df02d21216f13be8fe7d61f52d753339fd4
unknown <rencie.bautista@yahoo.com>
2015-05-25 12:09:43 +0800

    1

--- app/config/packages/barryvdh/laravel-snappy/config.php
+++ app/config/packages/barryvdh/laravel-snappy/config.php
@@ -5,7 +5,8 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+        // 'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+        'binary' => base_path('vendor/h4cc/wkhtmltopdf/bin/wkhtmltopdf.exe'),
         'timeout' => false,
         'options' => array(),
     ),
