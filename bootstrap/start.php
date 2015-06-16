a5f14301aae5e2bcea2d8f8f01f379e68ef45af5
unknown <rencie.bautista@yahoo.com>
2015-06-16 23:26:51 +0800

    1

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,8 +27,8 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('homestead','ULTRABOOK-PC'),
-	'development1' => array('www.allocation.chasetech.com'),
-	'development2' => array('www.allocation.ctcworkforce.com'),
+	'development1' => array('www.chasetech.com'),
+	'development2' => array('www.ctcworkforce.com'),
 
 ));
 
