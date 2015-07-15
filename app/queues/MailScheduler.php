c673273f47b13cae64d3b41e8a5191743e971033
unknown <rencie.bautista@yahoo.com>
2015-07-15 20:08:37 +0800

    1

--- app/queues/MailScheduler.php
+++ app/queues/MailScheduler.php
@@ -3,24 +3,24 @@
 class MailScheduler{
 	public function fire($job, $data){
 
-		$user = User::find(1);
-		$cycles = Cycle::getBySubmissionDeadline();
-		$cycle_ids = array();
-		foreach ($cycles as $value) {
-			$cycle_ids[] = $value->id;
-		}
-		$data['cycles'] = $cycles;
-		$data['user'] = $user->getFullname();
-		$data['email'] = $user->email;
-		$data['fullname'] = $user->getFullname();
-		$data['cycle_ids'] = $cycle_ids;
-		$data['activities'] = Activity::ProponentActivitiesForApproval($user->id,$cycle_ids);
-		Mail::send('emails.mail1', $data, function($message) {
-			$message->to("rbautista@chasetech.com", $data['fullname'])->subject('TOP ACTIVITY STATUS');
-		});
+		// $user = User::find(1);
+		// $cycles = Cycle::getBySubmissionDeadline();
+		// $cycle_ids = array();
+		// foreach ($cycles as $value) {
+		// 	$cycle_ids[] = $value->id;
+		// }
+		// $data['cycles'] = $cycles;
+		// $data['user'] = $user->getFullname();
+		// $data['email'] = $user->email;
+		// $data['fullname'] = $user->getFullname();
+		// $data['cycle_ids'] = $cycle_ids;
+		// $data['activities'] = Activity::ProponentActivitiesForApproval($user->id,$cycle_ids);
+		// Mail::send('emails.mail1', $data, function($message) {
+		// 	$message->to("rbautista@chasetech.com", $data['fullname'])->subject('TOP ACTIVITY STATUS');
+		// });
 
 		//File::append(storage_path().'/queue.txt',$data['user_id'].$job_id.PHP_EOL); //Add content to file
-		
+		Artisan::call('test:mail');
 		$job->delete();
 		return true;
 
