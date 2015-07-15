f1c2358d792951742533214ae605c8c89c51e7fe
unknown <rencie.bautista@yahoo.com>
2015-07-15 20:02:09 +0800

    1

--- app/queues/MailScheduler.php
+++ app/queues/MailScheduler.php
@@ -3,21 +3,21 @@
 class MailScheduler{
 	public function fire($job, $data){
 
-		// $user = User::find($data['user_id']);
-		// $cycles = Cycle::whereIn('id',$data['cycles']);
-		// $data2['cycles'] = $cycles;
-		// $data2['user'] = $user->getFullname();
-		// $data2['email'] = $user->email;
-		// $data2['fullname'] = $user->getFullname();
-		// $data2['activities'] = Activity::ProponentActivitiesForApproval($user->id,$data['cycles']);
+		$user = User::find($data['user_id']);
+		$cycles = Cycle::whereIn('id',$data['cycles']);
+		$data2['cycles'] = $cycles;
+		$data2['user'] = $user->getFullname();
+		$data2['email'] = $user->email;
+		$data2['fullname'] = $user->getFullname();
+		$data2['activities'] = Activity::ProponentActivitiesForApproval($user->id,$data['cycles']);
 
-		// Mail::send('emails.mail1', $data2, function($message) use ($data){
-		// 	$message->to("rbautista@chasetech.com", $data2['fullname'])->subject('TOP ACTIVITY STATUS');
-		// });
+		Mail::send('emails.mail1', $data2, function($message) use ($data){
+			$message->to("rbautista@chasetech.com", $data2['fullname'])->subject('TOP ACTIVITY STATUS');
+		});
 
-		File::append(storage_path().'/queue.txt',"hello world".PHP_EOL); //Add content to file
+		//File::append(storage_path().'/queue.txt',$data['user_id'].$job_id.PHP_EOL); //Add content to file
 		
-		// $job->delete();
+		$job->delete();
 		return true;
 
 	}
