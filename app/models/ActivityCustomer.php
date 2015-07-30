024bbf656683a0f409636d0c0b23358b52c2b47b
unknown <rencie.bautista@yahoo.com>
2015-07-30 15:33:42 +0800

    remove duplicate areas in areas

--- app/models/ActivityCustomer.php
+++ app/models/ActivityCustomer.php
@@ -19,13 +19,13 @@ class ActivityCustomer extends \Eloquent {
 			$_selected_customer = explode(".", $row);
 			if(!empty($_selected_customer[1])){
 				$sel_area = Area::where('area_code',$_selected_customer[1])->first();
-				$areas[] = $sel_area->area_name;
+				$areas[$sel_area->area_name] = $sel_area->area_name;
 			}else{
 				//get all areas
 				$sel_areas = Area::where('group_code',$_selected_customer[0])->get();
 				foreach ($sel_areas as $sel_area) {
 					// echo $sel_area->area_name;
-					$areas[] = $sel_area->area_name;
+					$areas[$sel_area->area_name] = $sel_area->area_name;
 				}
 
 			}
