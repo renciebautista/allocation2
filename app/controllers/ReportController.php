dcf523ed9f110e53b33e3343c0af6ba02bee5350
unknown <rencie.bautista@yahoo.com>
2015-07-30 17:43:25 +0800

    fix report

--- app/controllers/ReportController.php
+++ app/controllers/ReportController.php
@@ -15,11 +15,11 @@ class ReportController extends \BaseController {
 		$cycles = Cycle::getLists();
 		$scopes = ScopeType::getLists();
 		$types = ActivityType::getLists();
-		$planners = User::getApprovers(['PMOG PLANNER']);
-		$proponents = User::getApprovers(['PROPONENT']);
-		$activities = Activity::search(Input::get('pr'),Input::get('st'),Input::get('cy'),Input::get('sc'),
-			Input::get('ty'),Input::get('pm'),Input::get('title'));
-		return View::make('report.activities',compact('statuses', 'activities', 'cycles', 'scopes', 'types', 'planners', 'proponents'));
+		// $planners = User::getApprovers(['PMOG PLANNER']);
+		// $proponents = User::getApprovers(['PROPONENT']);
+		// $activities = Activity::search(Input::get('pr'),Input::get('st'),Input::get('cy'),Input::get('sc'),
+		// 	Input::get('ty'),Input::get('pm'),Input::get('title'));
+		// return View::make('report.activities',compact('statuses', 'activities', 'cycles', 'scopes', 'types', 'planners', 'proponents'));
 	}
 
 	public function preview($id){
