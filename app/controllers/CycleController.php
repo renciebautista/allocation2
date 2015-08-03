d38ec26e5151cbed858739b9b5006fd458bb15a1
unknown <rencie.bautista@yahoo.com>
2015-08-03 18:31:37 +0800

    test mail

--- app/controllers/CycleController.php
+++ app/controllers/CycleController.php
@@ -201,7 +201,6 @@ class CycleController extends \BaseController {
 	public function release(){
 		if(Request::ajax()){
 			$ids = Input::get('ids');
-
 			$users = User::GetPlanners(['PROPONENT' ,'PMOG PLANNER','GCOM APPROVER','CD OPS APPROVER','CMD DIRECTOR','FIELD SALES']);
 
 			$cycle_ids = array();
@@ -210,35 +209,37 @@ class CycleController extends \BaseController {
 					$cycle_ids[] = $value;
 				}
 			}
-			$total_activities = 0;
-			$type = "mail4";
-			$forRelease = Activity::where('activities.status_id','>',7)
-				->where('activities.pdf', 1)
-				->whereIn('activities.cycle_id',$cycle_ids)
-				->get();
-			if(count($forRelease) > 0){
-				$total_activities = count($forRelease);
-				foreach ($forRelease as $activity) {
-					$activity->scheduled = 1;
-					$activity->status_id = 9;
-					$activity->update();
-				}
 
-				foreach ($users as $user) {
-					$data['activities'] = Activity::Released($cycle_ids);
-					if(count($data['activities']) > 0){
-						if($_ENV['MAIL_TEST']){
-							Queue::push('MailScheduler', array('type' => $type, 'user_id' => $user->user_id, 'role_id' => $user->role_id,),'etop');
-						}else{
-							Queue::push('MailScheduler', array('type' => $type, 'user_id' => $user->user_id, 'role_id' => $user->role_id),'p_etop');
-						}
-					}
-				}
+			var_dump($cycle_ids);
+			// $total_activities = 0;
+			// $type = "mail4";
+			// $forRelease = Activity::where('activities.status_id','>',7)
+			// 	->where('activities.pdf', 1)
+			// 	->whereIn('activities.cycle_id',$cycle_ids)
+			// 	->get();
+			// if(count($forRelease) > 0){
+			// 	$total_activities = count($forRelease);
+			// 	foreach ($forRelease as $activity) {
+			// 		$activity->scheduled = 1;
+			// 		$activity->status_id = 9;
+			// 		$activity->update();
+			// 	}
+
+			// 	foreach ($users as $user) {
+			// 		$data['activities'] = Activity::Released($cycle_ids);
+			// 		if(count($data['activities']) > 0){
+			// 			if($_ENV['MAIL_TEST']){
+			// 				Queue::push('MailScheduler', array('type' => $type, 'user_id' => $user->user_id, 'role_id' => $user->role_id,),'etop');
+			// 			}else{
+			// 				Queue::push('MailScheduler', array('type' => $type, 'user_id' => $user->user_id, 'role_id' => $user->role_id),'p_etop');
+			// 			}
+			// 		}
+			// 	}
 				
-			}else{
-			}
+			// }else{
+			// }
 			
-			echo $total_activities;
+			// echo $total_activities;
 			
 		}
 
