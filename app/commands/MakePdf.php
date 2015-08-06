f990f79c870c3e164c164aa874b58663816a5fa8
unknown <rencie.bautista@yahoo.com>
2015-08-06 23:12:15 +0800

    update make pdf class

--- app/commands/MakePdf.php
+++ app/commands/MakePdf.php
@@ -263,11 +263,12 @@ class MakePdf extends Command {
 				}
 			}
 				
-
-			$pis_view = "";
-			$pis_view .= View::make('pdf.style')->render();
-			$pis_view .= View::make('pdf.pis',compact('activity','pis'))->render();
-			$pdf->writeHTML($pis_view , $ln=true, $fill=false, $reset=false, $cell=false, $align='');
+			if(count($pis) > 0){
+				$pis_view = "";
+				$pis_view .= View::make('pdf.style')->render();
+				$pis_view .= View::make('pdf.pis',compact('activity','pis'))->render();
+				$pdf->writeHTML($pis_view , $ln=true, $fill=false, $reset=false, $cell=false, $align='');
+			}
 
 			if(count($schemes) > 0){
 				$pdf->SetFont('helvetica', '', 6);
