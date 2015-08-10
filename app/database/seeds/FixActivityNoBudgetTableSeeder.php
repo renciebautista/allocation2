76885c196d494719f2d1102b9cfbf52b99b86abd
unknown <rencie.bautista@yahoo.com>
2015-08-10 11:38:29 +0800

    test seed

--- app/database/seeds/FixActivityNoBudgetTableSeeder.php
+++ app/database/seeds/FixActivityNoBudgetTableSeeder.php
@@ -7,7 +7,8 @@ class FixActivityNoBudgetTableSeeder extends Seeder {
 
 	public function run()
 	{
-		$nobudgets = ActivityNoBudget::all();
+		// $nobudgets = ActivityNoBudget::all();
+		$nobudgets = DB::table('activity_nobudgets')->get();
 		foreach ($nobudgets as $nobudget) {
 			$budgettype = BudgetType::find($nobudget->budget_type_id);
 			$nobudget->budget_desc = $budgettype->budget_type;
