96ab489ad805a1b03e25db08d3a224e447b937c0
unknown <rencie.bautista@yahoo.com>
2015-08-10 14:20:08 +0800

    test report

--- app/commands/MakeAllocReport.php
+++ app/commands/MakeAllocReport.php
@@ -51,61 +51,61 @@ class MakeAllocReport extends Command {
 
 		$this->line('Template name' . $template->name);
 
-		// $data['cycles'] = explode(",", $cycles);
-		// $data['status'] = AllocationReportFilter::getList($template->id,1);
-		// $data['scopes'] = AllocationReportFilter::getList($template->id,2);
-		// $data['proponents'] = AllocationReportFilter::getList($template->id,3);
-		// $data['planners'] = AllocationReportFilter::getList($template->id,4);
-		// $data['approvers'] = AllocationReportFilter::getList($template->id,5);
-		// $data['activitytypes'] = AllocationReportFilter::getList($template->id,6);
-		// $data['divisions'] = AllocationReportFilter::getList($template->id,7);
-		// $data['categories'] = AllocationReportFilter::getList($template->id,8);
-		// $data['brands'] = AllocationReportFilter::getList($template->id,9);
-		// $data['groups'] = AllocationReportFilter::getList($template->id,10);
-		// $data['areas'] = AllocationReportFilter::getList($template->id,11);
-		// $data['soldtos'] = AllocationReportFilter::getList($template->id,12);
-		// $data['shiptos'] = AllocationReportFilter::getList($template->id,13);
-		// $data['outlets'] = AllocationReportFilter::getList($template->id,14);
+		$data['cycles'] = explode(",", $cycles);
+		$data['status'] = AllocationReportFilter::getList($template->id,1);
+		$data['scopes'] = AllocationReportFilter::getList($template->id,2);
+		$data['proponents'] = AllocationReportFilter::getList($template->id,3);
+		$data['planners'] = AllocationReportFilter::getList($template->id,4);
+		$data['approvers'] = AllocationReportFilter::getList($template->id,5);
+		$data['activitytypes'] = AllocationReportFilter::getList($template->id,6);
+		$data['divisions'] = AllocationReportFilter::getList($template->id,7);
+		$data['categories'] = AllocationReportFilter::getList($template->id,8);
+		$data['brands'] = AllocationReportFilter::getList($template->id,9);
+		$data['groups'] = AllocationReportFilter::getList($template->id,10);
+		$data['areas'] = AllocationReportFilter::getList($template->id,11);
+		$data['soldtos'] = AllocationReportFilter::getList($template->id,12);
+		$data['shiptos'] = AllocationReportFilter::getList($template->id,13);
+		$data['outlets'] = AllocationReportFilter::getList($template->id,14);
 
-		// $token = md5(uniqid(mt_rand(), true));
+		$token = md5(uniqid(mt_rand(), true));
 		
-		// $timeFirst  = strtotime(date('Y-m-d H:i:s'));
-		// $filePath = storage_path('exports/'.$token.'.xlsx');
-		// $writer = WriterFactory::create(Type::XLSX);
-		// $writer->openToFile($filePath); // write data to a file or to a PHP stream
-		// $take = 1000; // adjust this however you choose
-		// $counter = 0; // used to skip over the ones you've already processed
-		// while($rows = AllocationReport::getReport($data,$take,$counter))
-		// {
-		// 	if(count($rows) == 0){
-		//     	break;
-		//     }
-		//     $counter += $take;
-		//     foreach($rows as $key => $value)
-		// 	{
-		// 	    $rows[$key] = (array) $value;
-		// 	} 
-		// 	$export_data = $rows;
+		$timeFirst  = strtotime(date('Y-m-d H:i:s'));
+		$filePath = storage_path('exports/'.$token.'.xlsx');
+		$writer = WriterFactory::create(Type::XLSX);
+		$writer->openToFile($filePath); // write data to a file or to a PHP stream
+		$take = 1000; // adjust this however you choose
+		$counter = 0; // used to skip over the ones you've already processed
+		while($rows = AllocationReport::getReport($data,$take,$counter))
+		{
+			if(count($rows) == 0){
+		    	break;
+		    }
+		    $counter += $take;
+		    foreach($rows as $key => $value)
+			{
+			    $rows[$key] = (array) $value;
+			} 
+			$export_data = $rows;
 
-		//     $writer->addRows($export_data); // add multiple rows at a time
-		// }
-		// $writer->close();
-		// $timeSecond = strtotime(date('Y-m-d H:i:s'));
-		// $differenceInSeconds = $timeSecond - $timeFirst;
-		// $this->line( 'Time used ' . $differenceInSeconds . " sec");
+		    $writer->addRows($export_data); // add multiple rows at a time
+		}
+		$writer->close();
+		$timeSecond = strtotime(date('Y-m-d H:i:s'));
+		$differenceInSeconds = $timeSecond - $timeFirst;
+		$this->line( 'Time used ' . $differenceInSeconds . " sec");
 
 		
-		// $newfile = new AllocationReportFile;
-		// $newfile->created_by = $user->id;
-		// $newfile->token = $token;
-		// $newfile->file_name = $token.'.xlsx';
+		$newfile = new AllocationReportFile;
+		$newfile->created_by = $user->id;
+		$newfile->token = $token;
+		$newfile->file_name = $token.'.xlsx';
 
-		// $data['template'] = $template;
-  //   	$data['token'] = $token;
-  //   	Mail::send('emails.allocreport', $data, function($message) use ($user){
-		// 	$message->to($user->email, $user->first_name);
-		// 	$message->subject('Allocation Report - '.$template->name);
-		// });
+		$data['template'] = $template;
+    	$data['token'] = $token;
+    	Mail::send('emails.allocreport', $data, function($message) use ($user){
+			$message->to($user->email, $user->first_name);
+			$message->subject('Allocation Report - '.$template->name);
+		});
 	}
 
 	/**
