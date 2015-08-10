e0dbeb5f4848c3aac356e4e2447cec37505429a1
unknown <rencie.bautista@yahoo.com>
2015-08-10 16:23:34 +0800

    adjust execution time

--- app/queues/AllocReportScheduler.php
+++ app/queues/AllocReportScheduler.php
@@ -2,6 +2,7 @@
 
 class AllocReportScheduler{
 	public function fire($job, $data){
+		set_time_limit(0);
 		Artisan::call('make:allocreport',array('temp_id' => $data['temp_id'],
 			'user_id' => $data['user_id'],
 			'cycles' => $data['cycles']));
