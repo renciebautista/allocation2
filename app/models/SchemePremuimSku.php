9c050ee51889f75ed480ec0e64e1d564b9b42006
unknown <rencie.bautista@yahoo.com>
2015-08-10 11:35:10 +0800

    test seed

--- app/models/SchemePremuimSku.php
+++ app/models/SchemePremuimSku.php
@@ -2,7 +2,7 @@
 
 class SchemePremuimSku extends \Eloquent {
 	protected $fillable = [];
-
+	public $timestamps = false;
 	public static function getPremuim($id){
 		$premuim = array();
 		foreach(self::where('scheme_id',$id)->get() as $host){
