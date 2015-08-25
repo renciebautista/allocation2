1ca0710eba3b1b25652ef2e1aaae7b2994e7c6d0
unknown <rencie.bautista@yahoo.com>
2015-08-25 12:54:17 +0800

    1

--- app/classes/WordDoc.php
+++ app/classes/WordDoc.php
@@ -495,12 +495,12 @@ class WordDoc {
 					}
 
 					$casecodeCell = $barcodeTable->addCell(5525);
-					if($scheme->item_casecode  !== ""){
-						DNS1D::getBarcodePNGPath($scheme->item_casecode, "I25",2,80);
-						$casecodeCell->addText($scheme->name,array('size' => 8,'align' => 'center'), $fontStyle);
-						$casecodeCell->addImage('public/barcode/'.$scheme->item_casecode.'.png',array('align' => 'center','spaceAfter' => 0));
-						$casecodeCell->addText($scheme->item_casecode,array('size' => 8,'align' => 'center'), $fontStyle);
-					}	
+					// if($scheme->item_casecode  !== ""){
+					// 	DNS1D::getBarcodePNGPath($scheme->item_casecode, "I25",2,80);
+					// 	$casecodeCell->addText($scheme->name,array('size' => 8,'align' => 'center'), $fontStyle);
+					// 	$casecodeCell->addImage('public/barcode/'.$scheme->item_casecode.'.png',array('align' => 'center','spaceAfter' => 0));
+					// 	$casecodeCell->addText($scheme->item_casecode,array('size' => 8,'align' => 'center'), $fontStyle);
+					// }	
 				}
 			}
 		}
