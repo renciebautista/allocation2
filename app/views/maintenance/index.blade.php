a9538c85f70b0ae6268701fde67009237185de25
unknown <rencie.bautista@yahoo.com>
2015-09-02 14:00:18 +0800

    add down page

---
+++ app/views/maintenance/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.login')
+
+@section('content')
+<br>
+<br>
+<br>
+<div class="row text-center">
+    <div class="col-md-12">
+        <div class="error-template">
+        	<span style="font-size: 90px;" class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span>
+            <h1 style="font-size: 30px;"> Site Maintenance</h1>
+            <h2>
+                We sincerely apologize for the inconvenience.</h2>
+            <div class="error-details">
+                Our website is currently undergoing scheduled maintenance and updgrades, but will return shortly. Thank you for your patience.
+            </div>
+        </div>
+    </div>
+</div>
+
+@stop
