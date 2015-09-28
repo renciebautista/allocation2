ed952a68aa951158b72866587325e99c5e1b1f30
Rencie Bautista <rencie.bautista@yahoo.com>
2015-09-28 11:34:20 +0800

    fix alloc report

--- app/queues/AllocReportScheduler.php
+++ app/queues/AllocReportScheduler.php
@@ -2,11 +2,14 @@
 
 class AllocReportScheduler{
 	public function fire($job, $data){
+		$job_id = $job->getJobId(); // Get job id
 		set_time_limit(0);
-		Artisan::call('make:allocreport',array('temp_id' => $data['temp_id'],
-			'user_id' => $data['user_id'],
-			'cycles' => $data['cycles']));
-		$job->delete();
+		// Artisan::call('make:allocreport',array('temp_id' => $data['temp_id'],
+		// 	'user_id' => $data['user_id'],
+		// 	'cycles' => $data['cycles']));
+		// $job->delete();
+
+		File::append(storage_path().'/queue.txt',$data['string'].$job_id.PHP_EOL); //Add content to file
 		return true;
 	}
 }	
