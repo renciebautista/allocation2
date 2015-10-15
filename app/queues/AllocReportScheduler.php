63bea4db92851d0fb8b7e6988fa0a79189ccffd3
Rencie Bautista <rencie.bautista@yahoo.com>
2015-10-16 00:55:52 +0800

    test report

--- app/queues/AllocReportScheduler.php
+++ app/queues/AllocReportScheduler.php
@@ -4,13 +4,13 @@ class AllocReportScheduler{
 	public function fire($job, $data){
 		$job_id = $job->getJobId(); // Get job id
 		set_time_limit(0);
-		// Artisan::call('make:allocreport',array('temp_id' => $data['temp_id'],
-		// 	'user_id' => $data['user_id'],
-		// 	'cycles' => $data['cycles'],
-		// 	'name' => $data['name']));
+		Artisan::call('make:allocreport',array('temp_id' => $data['temp_id'],
+			'user_id' => $data['user_id'],
+			'cycles' => $data['cycles'],
+			'name' => $data['name']));
 		$job->delete();
 
-		File::append(storage_path().'/queue.txt',$data['temp_id'].'=>'.$data['user_id'].'=>'.$data['cycles'].'=>'.$data['name'],'=>'.$job_id.PHP_EOL); //Add content to file
+		// File::append(storage_path().'/queue.txt',$data['temp_id'].'=>'.$data['user_id'].'=>'.$data['cycles'].'=>'.$data['name'],'=>'.$job_id.PHP_EOL); //Add content to file
 		return true;
 	}
 }	
