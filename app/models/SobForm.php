62dd99beb9b6f18dc931b99ed8cb74e4c68febe5
Rencie Bautista <rencie.bautista@yahoo.com>
2015-10-30 22:39:34 +0800

    fix sob dates

--- app/models/SobForm.php
+++ app/models/SobForm.php
@@ -195,7 +195,7 @@ class SobForm extends \Eloquent {
 					// $loading_date = $week_start->format('Y-m-d');
 					$date = date_create();
 					date_isodate_set($date, $soldto->year, $soldto->weekno,$shipTo->dayofweek);
-					$loading_date = date_format($date, 'Y-m-d');
+					$loading_date = date_format(date_create($date), 'Y-m-d');
 					// dD($loading_date);
 					
 					$receipt_date = date('Y-m-d', strtotime($loading_date . '+ 1 days'));
