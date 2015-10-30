1f8c101481fa17139f1c931f6234899e0016c3cf
Rencie Bautista <rencie.bautista@yahoo.com>
2015-10-30 22:59:25 +0800

    fix sob dates

--- app/models/SobForm.php
+++ app/models/SobForm.php
@@ -188,6 +188,7 @@ class SobForm extends \Eloquent {
 		       	// add ship to
 		       	$row = 8;
 		       	Helper::print_r($soldtos);
+		       	dd(1);
 		       	foreach ($soldtos as $soldto) {
 
 		       		$shipTo = ShipTo::where('ship_to_code',$soldto->ship_to_code)->first();
