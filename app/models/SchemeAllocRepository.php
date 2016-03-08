f1fcae1769dd4c00034b7fd7d78e570218a2ade4
Rencie Bautista <rencie.bautista@yahoo.com>
2016-03-08 16:56:25 +0800

    test schem update

--- app/models/SchemeAllocRepository.php
+++ app/models/SchemeAllocRepository.php
@@ -22,8 +22,8 @@ class SchemeAllocRepository
 		$_allocation = new AllocationRepository;
 		$allocations = $_allocation->customers($skus, $_channels, $customers,$forced_areas);
 		$_areasales =  $_allocation->area_sales();
-	   	Helper::print_r($allocations);
-	   	dd($allocations);
+	   	// Helper::print_r($allocations);
+	   	// dd($allocations);
 		$total_sales = $_allocation->total_gsv();
 		$force_total_sales = $_allocation->force_total_gsv();
 		$force_alloc = $activity->allow_force;
