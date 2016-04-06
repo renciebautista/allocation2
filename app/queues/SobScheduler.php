335a13caf48cd4ccd1cf919fa745259fcdc207ef
Rencie Bautista <rencie.bautista@yahoo.com>
2016-04-06 19:49:05 +0800

    udpate

--- app/queues/SobScheduler.php
+++ app/queues/SobScheduler.php
@@ -2,7 +2,7 @@
 
 class SobScheduler{
 	public function fire($job, $data){
-		Artisan::call('make:sob',array('cycle_ids' => explode(",", $data['cycle_ids'])));
+		Artisan::call('make:sob',array('cycle_ids' => implode(",", $data['cycle_ids'])));
 		$job->delete();
 		return true;
 	}
