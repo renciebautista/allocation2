106a25c634092b3d850765c16a593ab2bb81efe6
Rencie Bautista <rencie.bautista@yahoo.com>
2016-04-29 09:46:11 +0800

    add active on activity types

---
+++ app/database/migrations/2016_04_29_093646_add_active_on_activity_types_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddActiveOnActivityTypesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('activity_types', function(Blueprint $table)
+		{
+			$table->boolean('active')->default(1);
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('activity_types', function(Blueprint $table)
+		{
+			$table->dropColumn(['active']);
+		});
+	}
+
+}
