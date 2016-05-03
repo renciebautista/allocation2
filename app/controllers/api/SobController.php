9fcb15a1ec3cf8eb8c0221f40064e601a7a9b464
Rencie Bautista <rencie.bautista@yahoo.com>
2016-05-03 13:44:05 +0800

    udpate sob

--- app/controllers/api/SobController.php
+++ app/controllers/api/SobController.php
@@ -6,11 +6,14 @@ class SobController extends \BaseController {
 	public function weeks()
 	{
 		if(\Request::ajax()){
-	        	$year = Input::get('depdrop_all_params')['year'];
+	        $year = Input::get('depdrop_all_params')['year'];
 			$records = \AllocationSob::select('weekno as id', 'weekno as name')
+				->join('schemes', 'schemes.id', '=', 'allocation_sobs.scheme_id')
+				->join('activities', 'activities.id', '=', 'schemes.activity_id')
+				->where('activities.status_id', 9)
+				->where('year',$year)
 				->groupBy('weekno')
 				->orderBy('weekno')
-				->where('year',$year)
 				->get();
 			$data['output'] = $records;
 			$data['selected'] = Input::get('depdrop_all_params')['week_id'];
@@ -24,12 +27,13 @@ class SobController extends \BaseController {
 			$year = Input::get('depdrop_all_params')['year'];
 			$week = Input::get('depdrop_all_params')['week'];
 			$records =  \AllocationSob::select('activity_type_id as id', 'activitytype_desc as name')
-				->where('year',$year)
-				->where('weekno',$week)
-				->orderBy('activitytype_desc')
 				->join('schemes', 'schemes.id', '=', 'allocation_sobs.scheme_id')
 				->join('activities', 'activities.id', '=', 'schemes.activity_id')
+				->where('year',$year)
+				->where('weekno',$week)
+				->where('activities.status_id', 9)
 				->groupBy('activity_type_id')
+				->orderBy('activitytype_desc')
 				->get();
 			$data['output'] = $records;
 			$data['selected'] = Input::get('depdrop_all_params')['type_id'];
@@ -45,13 +49,14 @@ class SobController extends \BaseController {
 			$type = Input::get('depdrop_all_params')['activity_type'];
 
 			$records =  \AllocationSob::select('schemes.brand_shortcut as id', 'schemes.brand_shortcut as name')
-				->where('year',$year)
-				->where('weekno',$week)
-				->where('activity_type_id',$type)
-				->orderBy('brand_shortcut')
 				->join('schemes', 'schemes.id', '=', 'allocation_sobs.scheme_id')
 				->join('activities', 'activities.id', '=', 'schemes.activity_id')
+				->where('year',$year)
+				->where('weekno',$week)
+				->where('activities.status_id', 9)
+				->where('activity_type_id',$type)
 				->groupBy('brand_shortcut')
+				->orderBy('brand_shortcut')
 				->get();
 			$data['output'] = $records;
 			$data['selected'] = Input::get('depdrop_all_params')['brand_id'];
