57ad2b3b74edecab15599e4bae7ce1ee5ecaa2ee
Rencie Bautista <rencie.bautista@yahoo.com>
2016-09-19 18:24:18 +0800

    update test

--- app/database/seeds/PermissionTableSeeder.php
+++ app/database/seeds/PermissionTableSeeder.php
@@ -11,13 +11,22 @@ class PermissionTableSeeder extends Seeder {
 		['name' => 'manage_my_jo', 'display_name' => 'Manage My Job Orders']];
 		foreach ($permissions as $permission) {
 			// dd($permission['display_name']);
-			$per = Permission::where('name', $permission['name'])->get();
-			if(empty($per)){
+			$p = Permission::all();
+			if(count($p) > 0){
+				$per = Permission::where('name', $permission['name'])->get();
+				if(empty($per)){
+					$newPermission = new Permission;
+					$newPermission->name = $permission['name'];
+					$newPermission->display_name = $permission['display_name'];
+					$newPermission->save();
+				}
+			}else{
 				$newPermission = new Permission;
-				$newPermission->name = $permission['name'];
-				$newPermission->display_name = $permission['display_name'];
-				$newPermission->save();
+					$newPermission->name = $permission['name'];
+					$newPermission->display_name = $permission['display_name'];
+					$newPermission->save();
 			}
+			
 			
 		}
 	}
