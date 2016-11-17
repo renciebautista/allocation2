13daab8d5f7762fc6f98b6014c2cd410773c6d97
Rencie Bautista <rencie.bautista@yahoo.com>
2016-11-17 08:43:15 +0800

    update

--- app/database/migrations/2016_07_14_144423_create_tradedeal_types_table.php
+++ app/database/migrations/2016_07_14_144423_create_tradedeal_types_table.php
@@ -12,16 +12,17 @@ class CreateTradedealTypesTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('tradedeal_types', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->string('tradedeal_type');
-		});
+		// Schema::create('tradedeal_types', function(Blueprint $table)
+		// {
+		// 	$table->increments('id');
+		// 	$table->string('tradedeal_type');
+		// 	$table->boolean('active');
+		// });
 
-		DB::statement("INSERT INTO tradedeal_types (id, tradedeal_type, active) VALUES
-			(1, 'INDIVIDUAL', '1'),
-			(2, 'COLLECTIVE BASKET', '0'),
-			(3, 'COLLECTIVE', '1');");
+		// DB::statement("INSERT INTO tradedeal_types (id, tradedeal_type, active) VALUES
+		// 	(1, 'INDIVIDUAL', '1'),
+		// 	(2, 'COLLECTIVE BASKET', '0'),
+		// 	(3, 'COLLECTIVE', '1');");
 	}
 
 
