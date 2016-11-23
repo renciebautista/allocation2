ef2fbaf8d06c5e65ecc722f5fb3c6e3a32d53989
Rencie Bautista <rencie.bautista@yahoo.com>
2016-11-23 12:37:39 +0800

    add customer branch

---
+++ app/database/migrations/2016_11_23_123554_create_customer_branches_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCustomerBranchesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('customer_branches', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('customer_code');
+			$table->string('plant_code');
+			$table->string('branch_name');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('customer_branches');
+	}
+
+}
