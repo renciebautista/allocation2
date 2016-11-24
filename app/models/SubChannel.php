96dfb3e8082abf53d8fd291c05a2495de0fecacf
Rencie Bautista <rencie.bautista@yahoo.com>
2016-11-24 14:52:28 +0800

    udpate sub channels

--- app/models/SubChannel.php
+++ app/models/SubChannel.php
@@ -17,8 +17,14 @@ class SubChannel extends \Eloquent {
 	}
 
 	public static function getAll(){
-		return self::join('channels', 'channels.channel_code', '=' , 'sub_channels.channel_code')
-			->orderBy('channels.channel_code')
+		return self::select('sub_channels.id', 'coc_03_code', 'l3_desc', 'l4_code', 'l4_desc', 'l5_code', 'l5_desc', 'rtm_tag', 
+			'sub_channels.channel_code', 'channels.channel_name', 'trade_deal')
+			->join('channels', 'channels.channel_code', '=' , 'sub_channels.channel_code')
+			->orderBy('channels.channel_name')
+			->orderBy('l3_desc')
+			->orderBy('l4_desc')
+			->orderBy('l5_desc')
+			->orderBy('rtm_tag')
 			->get();
 	}
 
