8f0c229eacab0b8163713cf862acb79f6427e6e3
Rencie Bautista <rencie.bautista@yahoo.com>
2016-11-24 21:21:26 +0800

    udpate

--- app/views/javascript/activity/edit.blade.php
+++ app/views/javascript/activity/edit.blade.php
@@ -34,18 +34,6 @@ function getCustomer(){
 	});
 }
 
-function getChannel(){
-	$.ajax({
-		type: "GET",
-		url: "../../api/channelselected?id={{$activity->id}}",
-		success: function(data){
-			$.each(data, function(i, node) {
-				$("#tree4").fancytree("getTree").getNodeByKey(node).setSelected(true);
-			});
-		}
-	});
-}
-
 if(location.hash.length > 0){
 	var activeTab = $('[href=' + location.hash + ']');
 	activeTab && activeTab.tab('show');
@@ -518,15 +506,6 @@ $("#tree3").fancytree({
 
 		var keys = selRootKeys.join(".").split(".");
 		// console.log(keys);
-		/**if($.inArray('E1397', keys) != -1){
-			$("#tree4").fancytree("enable");
-			getChannel();
-		}else{
-			$("#tree4").fancytree("getTree").visit(function(node){
-		        node.setSelected(false);
-		    });
-			$("#tree4").fancytree("disable");
-		}**/
 		$("#customers").val(selRootKeys.join(", "));
 		
 		show_force_alloc();
@@ -537,11 +516,6 @@ $("#tree3").fancytree({
         if(data.targetType == "checkbox"){
         	//console.log(data.node.tree);
 	        var keys = data.node.key.split(".");
-	        /**if($.inArray('E1397', keys) != -1){
-				$("#tree4").fancytree("getTree").visit(function(node){
-			        node.setSelected(true);
-			    });
-			}**/
     	}
     }
 });
@@ -562,72 +536,6 @@ $("#btnCSelectAll").click(function(){
   	return false;
 });
 
-$("#tree4").fancytree({
-	extensions: [],
-	checkbox: true,
-	selectMode: 3,
-	source: {
-		url: "../../api/channels?id={{$activity->id}}"
-	},
-	select: function(event, data) {
-		// Get a list of all selected TOP nodes
-		var selRootNodes = data.tree.getSelectedNodes(true);
-		// ... and convert to a key array:
-		var selRootKeys = $.map(selRootNodes, function(node){
-		  return node.key;
-		});
-
-
-		var keys = selRootKeys.join(".").split(".");
-		$("#channels_involved").val(selRootKeys.join(", "));
-	},
-	click: function(event, data) {
-        $("#updateCustomer").addClass("dirty");
-    },
-});
-
-$("#btnChDeselectAll").click(function(){
-	if(!$("#tree4").hasClass( "ui-fancytree-disabled" )){
-		$("#tree4").fancytree("getTree").visit(function(node){
-	    	node.setSelected(false);
-	  	});
-	}
-  	$("#updateCustomer").addClass("dirty");
-  	return false;
-});
-$("#btnChSelectAll").click(function(){
-	if(!$("#tree4").hasClass( "ui-fancytree-disabled" )){
-		$("#tree4").fancytree("getTree").visit(function(node){
-	    	node.setSelected(true);
-	  	});
-	}
-$("#updateCustomer").addClass("dirty");
-  	
-  	return false;
-});
-
-function updatechannel(){
-	$.ajax({
-		type: "GET",
-		url: "{{ URL::action('ActivityController@channels', $activity->id ) }}",
-		success: function(data){
-			$('select#channel').empty();
-			$.each(data.selection, function(i, text) {
-				var sel_class = '';
-				if(data.selected.length > 0){
-					if($.inArray( i,data.selected ) > -1){
-						sel_class = 'selected="selected"';
-					}
-				}else{
-					sel_class = 'selected="selected"';
-				}
-				
-				$('<option '+sel_class+' value="'+i+'">'+text+'</option>').appendTo($('select#channel')); 
-			});
-		$('select#channel').multiselect('rebuild');
-	   }
-	});
-}
 
 
 $("form[id='updateCustomer']").on("submit",function(e){
@@ -680,6 +588,7 @@ $('#allow_force').click(function() {
         $('#force_alloc').find('input').attr('disabled','disabled').addClass('disabled-readonly');
     }
 });
+
 
 function show_force_alloc(){
 	var a = [];
