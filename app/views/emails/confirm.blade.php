196c92d18bffcde0bfcbd809dff6a59abb8093e5
Rencie Bautista <rencie.bautista@yahoo.com>
2017-01-27 17:04:08 +0800

    udpate

---
+++ app/views/emails/confirm.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+  <title>ETOP Signup</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=us-ascii"><!-- CREATE TEXT STYLES USED IN THIS HTML FILE, START -->
+
+  <style type="text/css">
+  body{
+    font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif; 
+    font-size: 13px;
+  }
+
+  </style><!-- CREATE TEXT STYLES USED IN THIS HTML FILE, END -->
+</head>
+<body>
+  <h1>Hi</h1>
+
+<p>Uploading sales masterfile is complete.</p>
+
+<br>
+<p>Thanks,</p>
+<p>ETOP Administrator</p>
+</body>
+
+
+</html>
