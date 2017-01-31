00ee166ac875b7320657f457272f5386d0f19443
Rencie Bautista <rencie.bautista@yahoo.com>
2017-01-31 17:37:52 +0800

    add option to append sales

---
+++ app/database/seeds/MtDtSalesAppendTableSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+// Composer: "fzaninotto/faker": "v1.3.0"
+use Flynsarmy\CsvSeeder\CsvSeeder;
+
+class MtDtSalesAppendTableSeeder extends Seeder {
+
+	public function __construct()
+	{
+		$this->table = 'mt_dt_sales';
+		$folderpath = app_path().'/database/seeds/seed_files/Sales/';
+		$folders = File::directories($folderpath);
+		$latest = '11232015';
+		foreach ($folders as $value) {
+			$_dir = explode("/", $value);
+			$cnt = count($_dir);
+			$name = $_dir[$cnt - 1];
+			$latest_date = DateTime::createFromFormat('mdY', $latest);
+			$now = DateTime::createFromFormat('mdY', $name);
+			if($now > $latest_date){
+				$latest = $name;
+			}
+		}
+		$this->filename = $folderpath.$latest.'/append_sales.csv';
+
+		$this->offset_rows = 1;
+	    $this->mapping = [
+	        2 => 'area_code',
+	        4 => 'customer_code',
+	        6 => 'distributor_code',
+	        9 => 'plant_code',
+	        10 => 'account_name',
+	        12 => 'coc_03_code',
+	        14 => 'coc_04_code',
+	        16 => 'coc_05_code',
+	       	18 => 'child_sku_code',
+	       	20 => 'gss',
+	       	21 => 'gsv',
+	    ];
+
+		echo $latest .'-->'.$this->filename.PHP_EOL; 
+
+
+	}
+
+	public function run()
+	{
+		// Recommended when importing larger CSVs
+		DB::disableQueryLog();
+		parent::run();
+	}
+
+}
